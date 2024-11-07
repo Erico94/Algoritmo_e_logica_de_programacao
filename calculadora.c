8e77e0e4298c19a97764c73ed43216a84752fab3
Erico <ericoam.mussulin5@gmail.com>
2024-11-07 17:57:30 -0300

    calculadora em c

---
+++ calculadora.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int ativa = 0;
+
+    printf("\nDigite 1 para ligar a calculadora ou 0 para manter desligada.\n");
+    scanf("%d", &ativa);
+    while (ativa == 1.0)
+    {
+        float segundoNumero = 0, primeiroNumero = 0;
+        char operador;
+        float total = 0;
+
+        printf("\nDigite um numero, o operador e o segundo numero, sem espacos entre eles.\n");
+        printf("Ex: 2+7\n");
+        scanf("%f%c%f", &primeiroNumero, &operador, &segundoNumero);
+
+        if (operador == '+')
+        {
+            total = primeiroNumero + segundoNumero;
+            printf("\nVoce digitou %.2f %c %.2f e o resultado eh: %.2f\n", primeiroNumero, operador,
+             segundoNumero, total);
+        }
+        else if (operador == '-')
+        {
+            total = primeiroNumero - segundoNumero;
+            printf("\nVoce digitou %.2f %c %.2f e o resultado eh: %.2f\n", primeiroNumero, operador,
+             segundoNumero, total);
+        }
+        else if (operador == '/')
+        {
+            total = primeiroNumero / segundoNumero;
+            printf("\nVoce digitou %.2f %c %.2f e o resultado eh: %.2f\n", primeiroNumero, operador,
+             segundoNumero, total);
+        }
+        else if (operador == '*')
+        {
+            total = primeiroNumero * segundoNumero;
+            printf("\nVoce digitou %.2f %c %.2f e o resultado eh: %.2f\n", primeiroNumero, operador,
+             segundoNumero, total);
+        }
+        else
+            printf("\nOperador desconhecido.\n");
+
+        printf("Digite 1 para ligar a calculadora ou 0 para manter desligada.\n");
+        scanf("%d", &ativa);
+    }
+    return 0;
+}
